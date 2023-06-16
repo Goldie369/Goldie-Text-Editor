c4d95ae8e55f7b6165c1f7d5033354f839c9992d
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-06-15 21:36:21 -0500

    adding a comments for better understanding

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
+
 
 window.addEventListener('beforeinstallprompt', (event) => {
 
-    // store events
+    
     window.deferredPrompt = event;
     
-    // remove btn class
+    
     butInstall.classList.toggle('hidden', false);
 
 });
@@ -22,7 +22,7 @@ butInstall.addEventListener('click', async () => {
 
     promptEvent.prompt();
     
-    // reset the deferred prompt var
+    
     window.deferredPrompt = null;
     
     butInstall.classList.toggle('hidden', true);
